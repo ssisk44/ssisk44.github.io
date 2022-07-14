5f7c540ce939ab4252dd7b1c07960fa827c3f068
ssisk44 <65787613+ssisk44@users.noreply.github.com>
2022-07-14 19:02:08 -0400

    more minor changes + black background

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 
 let nav = document.querySelector("nav");
 let scrollBtn = document.querySelector(".scroll-button a");
-
 let val;
 
 window.onscroll = function() {
@@ -19,26 +18,8 @@ window.onscroll = function() {
 let body = document.querySelector("body");
 let navBar = document.querySelector(".navbar");
 let menuBtn = document.querySelector(".menu-btn");
-let cancelBtn = document.querySelector(".cancel-btn");
-
-menuBtn.onclick = function() {
-  navBar.classList.add("active");
-  menuBtn.style.opacity = "0";
-  menuBtn.style.pointerEvents = "none";
-  body.style.overflowX = "hidden";
-  scrollBtn.style.pointerEvents = "none";
-}
-
-cancelBtn.onclick = function() {
-  navBar.classList.remove("active");
-  menuBtn.style.opacity = "1";
-  menuBtn.style.pointerEvents = "auto";
-  body.style.overflowX = "auto";
-  scrollBtn.style.pointerEvents = "auto";
-}
 
 // Side Navigation Bar Close While We click On Navigation Links
-
 let navLinks = document.querySelectorAll(".menu li a");
 for (var i = 0; i < navLinks.length; i++) {
   navLinks[i].addEventListener("click" , function() {
