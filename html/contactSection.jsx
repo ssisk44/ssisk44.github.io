7db3a56e50ddf3119d9b7e7c458cc4d34ec8145d
ssisk44 <65787613+ssisk44@users.noreply.github.com>
2022-07-19 01:22:28 -0400

    more changes

--- html/contactSection.jsx
+++ html/contactSection.jsx
@@ -6,7 +6,7 @@
    <div class="topic">Have Any Projects?</div>
        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam neque ipsum corrupti dolores, facere numquam voluptate aspernatur sit perferendis qui nisi modi! Recusandae deserunt consequatur voluptatibus alias repellendus nobis eligendi.</p>
    <div class="button">
-   <button onclick="location.href='https://mail.google.com/mail/u/0/?fs=1&to=samuelsisk444@gmail.com@&tf=cm'" type="button">Email Me</button>
+   <button onclick="location.href='https://mail.google.com/mail/u/0/?fs=1&to=samuelsisk444@gmail.com&tf=cm'" type="button">Email Me</button>
    </div>
     </div>
     </div>
