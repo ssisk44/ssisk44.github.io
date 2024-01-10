4f38fa3f697468d8d69f6b5909300a852957268f
Samuel Sisk <samuelsisk444@gmail.com>
2024-01-10 17:14:45 -0500

    v1.3

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'ssisk44.github.io',
+  base: '/ssisk44.github.io/',
   plugins: [react()],
   build: {
     outDir: './dist'
   }
-
 })
